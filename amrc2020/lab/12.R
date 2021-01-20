cf4ac9ce5fb4c2b6b5385ac66fbe84bace9d4fe7
lazafi <lazafi@gmail.com>
2021-01-20 19:13:41 +0100

    c

--- amrc2020/lab/12.R
+++ amrc2020/lab/12.R
@@ -1,6 +1,6 @@
 # lab 12
 
-bank <- read.csv2("data/bank.csv")
+bank <- read.csv2("data/bank.csv", stringsAsFactors = TRUE)
 str(bank)
 
 set.seed(1234)
@@ -13,17 +13,19 @@ evalu <- function(rf) {
   error <- 1 - sum(diag(TAB)) / sum(TAB)
   ye <- TAB[1,2] / (TAB[1,1] + TAB[1,2])  
   ne <- TAB[2,1] / (TAB[2,1] + TAB[2,2])  
-
   res <- cbind(TAB, c(ye,ne))
-
-  rbind(res, c(NA,NA,error))
+  ret <- rbind(res, c(NA,NA,error))
+  dimnames(ret) <- list(dimnames(res)[[2]], c(dimnames(res)[[1]], 'error')) 
+  print(ret, na.print="")
   }
 
 # 1)
 
+install.packages("randomForest")
 library(randomForest)
-rf1 <- randomForest(y~., data=bank, subset=train, importance=TRUE)
+#?randomForest
 
+rf1 <- randomForest(y~., data=bank, subset=train, importance=TRUE)
 plot(rf1)
 varImpPlot(rf1)
 
@@ -38,10 +40,8 @@ evalu(rf1)
 ## sample same 'n' as 'y'
 y_idx <-train[bank[train,'y'] == "yes"]
 n_idx <- sample(train[bank[train,'y'] == "no"], length(y_idx))
+evalu(randomForest(y~., data=bank, subset=c(y_idx, n_idx)))
 
-rf2_1 <- randomForest(y~., data=bank, subset=c(y_idx, n_idx))
-
-evalu(rf2_1)
 
 # b)
 
@@ -52,6 +52,7 @@ evalu(randomForest(y~., data=bank, subset=train, sampsize=100))
 evalu(randomForest(y~., data=bank, subset=train, sampsize=200))
 evalu(randomForest(y~., data=bank, subset=train, sampsize=300))
 evalu(randomForest(y~., data=bank, subset=train, sampsize=400))
+
 evalu(randomForest(y~., data=bank, subset=train, sampsize=1000))
 
 # c)
@@ -59,3 +60,33 @@ evalu(randomForest(y~., data=bank, subset=train, sampsize=1000))
 evalu(randomForest(y~., data=bank, subset=train, classwt=c(length(n_idx)/length(y_idx),1)))
 
 # d)
+
+n <- length(train[bank[train,'y'] == "no"])
+y <-  length(train[bank[train,'y'] == "yes"])
+
+evalu(randomForest(y~., data=bank, subset=train, cutoff=c(1/y,1/n) ))
+evalu(randomForest(y~., data=bank, subset=train, cutoff=c(2/y,1/n) ))
+evalu(randomForest(y~., data=bank, subset=train, cutoff=c(3/y,1/n) ))
+
+# e)
+
+evalu(randomForest(y~., data=bank, subset=train, strata=default ))
+evalu(randomForest(y~., data=bank, subset=train, strata=y ))
+evalu(randomForest(y~., data=bank, subset=train, strata=job ))
+
+# f)
+
+#install.packages("DMwR")
+library(DMwR)
+
+stm <- SMOTE(y~., bank[train,])
+stm <- SMOTE(y~., bank[train,], perc.over = 300)
+table(stm$y)
+
+evalu(randomForest(y~., data=stm))
+varImpPlot(randomForest(y~., data=stm, importance=TRUE))
+
+
+
+
+
